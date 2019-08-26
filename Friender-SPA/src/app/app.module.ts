752658e57252b0b629e9adf31320a4dece9b2b1f
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-08-26 21:17:47 +0300

    Create Navigation Component with links and login form

--- Friender-SPA/src/app/app.module.ts
+++ Friender-SPA/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent
    ],
    imports: [
       BrowserModule,
