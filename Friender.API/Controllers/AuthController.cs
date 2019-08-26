fc067e70dd318277f76935ce1fe9e2e890e36389
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-08-26 12:25:40 +0300

    Create UserForRegister DTO and modify Auth Controller to use it

--- Friender.API/Controllers/AuthController.cs
+++ Friender.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Friender.API.Data;
+using Friender.API.Dtos;
 using Friender.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,21 +17,21 @@ namespace Friender.API.Controllers
     }
 
     [HttpPost("register")]
-    public async Task<IActionResult> Register(string username, string password)
+    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
     {
         // TODO: validate request
 
-        username = username.ToLower();        
+        userForRegisterDto.Username = userForRegisterDto.Username.ToLower();        
         
-        if (await _repo.UserExists(username))
+        if (await _repo.UserExists(userForRegisterDto.Username))
             return BadRequest("Username already exists.");
         
         var userToCreate = new User
         {
-            Username = username
+            Username = userForRegisterDto.Username
         };
 
-        var createdUser = await _repo.Register(userToCreate, password);
+        var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
         return StatusCode(201);
     }
