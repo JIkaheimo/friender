ab4be20c91a50b51386944bd2b1839f2fca243eb
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-08-26 11:55:17 +0300

    Create Register Method in Auth Controller

--- Friender.API/Controllers/AuthController.cs
+++ Friender.API/Controllers/AuthController.cs
@@ -1,11 +1,38 @@
+using System.Threading.Tasks;
+using Friender.API.Data;
+using Friender.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Friender.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _repo;
+    public AuthController(IAuthRepository repo)
     {
-        
+      _repo = repo;
     }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(string username, string password)
+    {
+        // TODO: validate request
+
+        username = username.ToLower();        
+        
+        if (await _repo.UserExists(username))
+            return BadRequest("Username already exists.");
+        
+        var userToCreate = new User
+        {
+            Username = username
+        };
+
+        var createdUser = await _repo.Register(userToCreate, password);
+
+        return StatusCode(201);
+    }
+   }
 }
